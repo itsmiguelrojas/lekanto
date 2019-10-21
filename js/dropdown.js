346a542959ba14947eeb637cd72a9a734e0c1b93
itsmiguelrojas <42596112+itsmiguelrojas@users.noreply.github.com>
2019-10-21 14:48:04 -0400

    Delete dropdown.js

--- js/dropdown.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(document).on('.dropdown', 'show.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true,true).slideDown(300);
-  });
-
-  $(document).on('.dropdown', 'hide.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true,true).slideUp(300);
-  });
-});
